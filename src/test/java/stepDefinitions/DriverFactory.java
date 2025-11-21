f18cd258fb15b3d887235d16cb4293a63d9dd10c
MutationStatus3300# <mutationstatus@gmail.com>
2025-11-22 01:42:25 +0530

    updated code

--- src/test/java/stepDefinitions/DriverFactory.java
+++ src/test/java/stepDefinitions/DriverFactory.java
@@ -2,6 +2,8 @@ package stepDefinitions;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class DriverFactory {
@@ -10,9 +12,18 @@ public class DriverFactory {
 
 	public static WebDriver getDriver() {
 		if (driver == null) {
+
 			WebDriverManager.chromedriver().setup();
+
+			ChromeOptions options = new ChromeOptions();
+			options.addArguments("--headless=new"); // new headless mode
+			options.addArguments("--no-sandbox"); // required in CI environments
+			options.addArguments("--disable-dev-shm-usage"); // prevent shared memory issues
+			options.addArguments("--disable-gpu"); // optional for headless
+			options.addArguments("--window-size=1920,1080");
+
 			// Launch the chrome browser
-			driver = new ChromeDriver();
+			driver = new ChromeDriver(options);
 		}
 		return driver;
 	}
