d4a1817aacc5b22feb9e870da6287debef5357c6
MutationStatus3300# <mutationstatus@gmail.com>
2025-11-22 20:23:24 +0530

    l

--- src/test/java/stepDefinitions/RegisterStepDef.java
+++ src/test/java/stepDefinitions/RegisterStepDef.java
@@ -50,7 +50,7 @@ public class RegisterStepDef {
 		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(300));
 		System.out.println("Launching web page");
 		//driver.get("https://www.landrecords.karnataka.gov.in/service2/RTC.aspx");
-		driver.get("https://landrecords.karnataka.gov.in/Service2/");
+		driver.get("https://www.google.com/");
 		
 		
 		driver.manage().window().maximize();
