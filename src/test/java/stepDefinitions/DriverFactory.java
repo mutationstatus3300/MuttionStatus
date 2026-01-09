289199885212471b79c58812dabac374fedb21ab
MutationStatus3300# <mutationstatus@gmail.com>
2026-01-09 22:12:07 +0530

    updated code

--- src/test/java/stepDefinitions/DriverFactory.java
+++ src/test/java/stepDefinitions/DriverFactory.java
@@ -21,15 +21,15 @@ public class DriverFactory {
 
 	        // Chrome options for headless mode
 	        ChromeOptions options = new ChromeOptions();
-	        Proxy proxy = new Proxy();
-	        proxy.setHttpProxy("139.84.217.118");  // Set HTTP proxy
-	        proxy.setSslProxy("139.84.217.118");   // Set HTTPS proxy
-	        options.addArguments("--headless=new"); // headless mode
-	        options.addArguments("--disable-gpu");
-	        options.addArguments("--no-sandbox");
-	        options.addArguments("--disable-dev-shm-usage"); // CI memory issues
-	        options.addArguments("--window-size=1920,1080");
-	        options.setProxy(proxy);
+	        options.addArguments("--headless=new");
+			/*
+			 * Proxy proxy = new Proxy(); proxy.setHttpProxy("139.84.217.118"); // Set HTTP
+			 * proxy proxy.setSslProxy("139.84.217.118"); // Set HTTPS proxy
+			 * options.addArguments("--headless=new"); // headless mode
+			 * options.addArguments("--disable-gpu"); options.addArguments("--no-sandbox");
+			 * options.addArguments("--disable-dev-shm-usage"); // CI memory issues
+			 * options.addArguments("--window-size=1920,1080"); options.setProxy(proxy);
+			 */
 
 	         driver = new ChromeDriver(options);
 			
